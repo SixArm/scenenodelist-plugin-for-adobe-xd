2708ed8d7560496794e45561c2513abe8a285bef
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-10-20 12:47:15 +0200

    Add functions for more node instance types

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ function sceneNodeListAsTextHandler(selection, documentRoot) {
 // Return the HTML.
 function nodeAsHTML(node) {
     if (node instanceof Artboard)  return nodeArtboardAsHTML(node)
-    if (node instanceof Ellipse)   return ""
+    if (node instanceof Ellipse)   return nodeEllipseAsHTML(node)
     if (node instanceof Group)     return nodeGroupAsHTML(node)
-    if (node instanceof Line)      return ""
-    if (node instanceof Path)      return ""
-    if (node instanceof Polygon)   return ""
-    if (node instanceof Rectangle) return ""
+    if (node instanceof Line)      return nodeLineAsHTML(node)
+    if (node instanceof Path)      return nodePathAsHTML(node)
+    if (node instanceof Polygon)   return nodePolygonAsHTML(node)
+    if (node instanceof Rectangle) return nodeRectangleAsHTML(node)
     if (node instanceof Text)      return nodeTextAsHTML(node)
     return nodeDefaultAsHTML(node)
 }
@@ -31,40 +31,105 @@ function nodeAsHTML(node) {
 // Return the text.
 function nodeAsText(node) {
     if (node instanceof Artboard)  return nodeArtboardAsText(node)
-    if (node instanceof Ellipse)   return ""
+    if (node instanceof Ellipse)   return nodeEllipseAsText(node)
     if (node instanceof Group)     return nodeGroupAsText(node)
-    if (node instanceof Line)      return ""
-    if (node instanceof Path)      return ""
-    if (node instanceof Polygon)   return ""
-    if (node instanceof Rectangle) return ""
+    if (node instanceof Line)      return nodeLineAsText(node)
+    if (node instanceof Path)      return nodePathAsText(node)
+    if (node instanceof Polygon)   return nodePolygonAsText(node)
+    if (node instanceof Rectangle) return nodeRectangleAsText(node)
     if (node instanceof Text)      return nodeTextAsText(node)
     return nodeDefaultAsText(node)
 }
 
+///
+// Render a node as HTML or text.
+//
+// This functions are indpendent in order to make them easy to edit,
+// such as a developer customizing this source code for new goals.
+///
+
+// Render a node that is an artboard as HTML.
 function nodeArtboardAsHTML(node) {
     return nodeDefaultAsHTML(node)   
 }
 
+// Render a node that is an artboard node as text.
 function nodeArtboardAsText(node) {
     return nodeDefaultAsText(node)   
 }
 
+// Render a node that is an ellipse as HTML.
+function nodeEllipseAsHTML(node) {
+    return ""
+}
+
+// Render a node that is an ellipse as text.
+function nodeEllipseAsText(node) {
+    return ""
+}
+
+// Render a node that is a group as HTML.
 function nodeGroupAsHTML(node) {
     return nodeDefaultAsHTML(node)
 }
 
+// Render a node that is a group as text.
 function nodeGroupAsText(node) {
     return nodeDefaultAsText(node)
 }
 
+// Render a node that is a line as HTML.
+function nodeLineAsHTML(node) {
+    return ""
+}
+
+// Render a node that is a line as text.
+function nodeLineAsText(node) {
+    return ""
+}
+
+// Render a node that is a path as HTML.
+function nodePathAsHTML(node) {
+    return ""
+}
+
+// Render a node that is a path as text.
+function nodePathAsText(node) {
+    return ""
+}
+
+// Render a node that is a polygon as HTML.
+function nodePolygonAsHTML(node) {
+    return ""
+}
+
+// Render a node that is a polygon as text.
+function nodePolygonAsText(node) {
+    return ""
+}
+
+// Render a node that is a rectangle as HTML.
+function nodeRectangleAsHTML(node) {
+    return ""
+}
+
+// Render a node that is a rectangle as text.
+function nodeRectangleAsText(node) {
+    return ""
+}
+
+// Render a node that is text as HTML.
 function nodeTextAsHTML(node) {
     return nodeDefaultAsHTML(node)
 }
 
+// Render a node that is text as text.
 function nodeTextAsText(node) {
     return nodeDefaultAsText(node)
 }
 
+// Render a node that is any kind as HTML.
+// This implementation renders the node's content and children.
 function nodeDefaultAsHTML(node) {
     let contentHTML = oneline(node.name)
     if (contentHTML != "") {
@@ -93,6 +158,8 @@ function nodeDefaultAsHTML(node) {
         "</div>\n"
 }
 
+// Render a node that is any kind as text.
+// This implementation renders the node's content and children.
 function nodeDefaultAsText(node) {
     let contentText = node.constructor.name + ": " + oneline(node.name) + "\n"
     if (contentText != "") {
